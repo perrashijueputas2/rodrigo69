92977dba42cc7047ed81ffa8682e77fcb3971be7
perrashijueputas2 <49006875+perrashijueputas2@users.noreply.github.com>
2019-04-05 20:42:35 -0500

    hoy si :3

--- LSMADH/Assets/Scenes/Cuenta.cs
+++ LSMADH/Assets/Scenes/Cuenta.cs
@@ -3,47 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//public class Cuenta
-//{
-    
-//    private double saldo;
-//    private string titular;
-//    public double GetSaldo()
-//    {
-//        return saldo;
-//    }
-//    public void SetSaldo(double _saldo)
-//    {
-//        this.saldo = _saldo;
-//    }
-//    public string GetTitular()
-//    {
-//        return titular;
-//    }
-//    public void SetTitular(string _titular)
-//    {
-//        this.titular = _titular;
-//    }
-//    public void Ingresar(double _valorAingresar)
-//    {
-//        if (saldo > 0)
-//        {
-//            Debug.Log("este es el nuevo saldo " + saldo);
-//        }
-//    }
-//    public void Retirar(double _valorARetirar)
-//    {
-//        Debug.Log("solo pudiste retirar " + saldo);
-//        Debug.Log("retiraste " + " y te quedó " + saldo + " en tu cuenta");
-//        saldo = 0;
-//    }
-//    public Cuenta(string _nombre, double _saldo)
-//    {
-//        this.saldo = _saldo;
-//        this.titular = _nombre;
-//    }
-//    public Cuenta(string _nombre)
-//    {
-//        this.titular = _nombre;
-//    }
-//}
+public class Cuenta
+{
+
+    private double saldo;
+    private string titular;
+    public double GetSaldo()
+    {
+        return saldo;
+    }
+    public void SetSaldo(double _saldo)
+    {
+        this.saldo = _saldo;
+    }
+    public string GetTitular()
+    {
+        return titular;
+    }
+    public void SetTitular(string _titular)
+    {
+        this.titular = _titular;
+    }
+    public void Ingresar(double _valorAingresar)
+    {
+        if (saldo > 0)
+        {
+            Debug.Log("este es el nuevo saldo " + saldo);
+        }
+    }
+    public void Retirar(double _valorARetirar)
+    {
+        Debug.Log("solo pudiste retirar " + saldo);
+        Debug.Log("retiraste " + " y te quedó " + saldo + " en tu cuenta");
+        saldo = 0;
+    }
+    public Cuenta(string _nombre, double _saldo)
+    {
+        this.saldo = _saldo;
+        this.titular = _nombre;
+    }
+    public Cuenta(string _nombre)
+    {
+        this.titular = _nombre;
+    }
+}
--- LSMADH/Assets/Scenes/GameManager.cs
+++ LSMADH/Assets/Scenes/GameManager.cs
@@ -3,52 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//public class GameManager: MonoBehaviour
-//{
-//    public InputField Saldo;
-//    public InputField Nombre;
-//    public Button Limpiar;
-//    public Button MostrarCuentaAnterior;
-//    public Button GuardarCuenta;
-//    public string nombreTemporal;
-//    public float temporal;
-//    int count;
-//    Cuenta[] cuentas = new Cuenta[2];
-//    private void Start()
-//    {
-//        count = 0;
-//    }
-//    private void Update()
-//    {
-//        if (count< cuentas.Length)
-//        {
-//            Debug.Log("Buenas buenas, éste es el creador de cuentas");
+public class GameManager : MonoBehaviour
+{
+    public InputField Saldo;
+    public InputField Nombre;
+    public Button MostrarCuentaAnterior;
+    public Button GuardarCuenta;
+    int count;
+    Cuenta[] cuentas = new Cuenta[2];
+    private void Start()
+    {
+        count = 0;
+        
+    }
+    private void Update()
+    {
+        if (count < cuentas.Length)
+        {
+            
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Debug.Log("Ingrese la cantidad");
+            }
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                Debug.Log("Presione guardar cuenta");
 
-//        }
-//    }
-//    public void Main()
-//    {
+            }
+        }
+    }
+    //strin.isnullorempty
+    public void DecidirGuardado()
+    {
+        if (!string.IsNullOrEmpty(Saldo.text))
+        {
+            Cuenta micuenta = new Cuenta(Nombre.text, System.Convert.ToDouble(Saldo.text));
+            Debug.Log("La cuenta con saldo ha sido creada");
+            Saludo();
+        }
+        else
+        {
+            Cuenta micuenta = new Cuenta(Nombre.text);
+            Debug.Log("La cuenta sin saldo ha sido creada");
+            Saludo();
+        }
+    }
+    public void Saludo()
+    {
+        if (count < cuentas.Length)
+        {
+            Debug.Log("Buenas buenas, éste es el creador de cuentas");
+            Debug.Log("Ingrese nombre del titular");
+            Debug.Log("Desea Crear cuenta con saldo? s = si, n = no");
+        }
+        else
+        {
+            Debug.Log("no tenemos capacidad prro :´v");
+        }
+    }
+    public void Main()
+    {
         
         
-       
-        
-//            Debug.Log("Ingrese nombre del titular");
-//            Debug.Log("Desea Crear cuenta con saldo? s = si, n = no");
-//            Cuenta miCuenta;
+        //Cuenta miCuenta;
 
-//            if (res == "s")
-//            {
-//                Debug.Log("Ingrese la cantidad");
-//                string saldo = Debug.Log();
-//                miCuenta = new Cuenta(nombre, Convert.ToDouble(saldo));
-//            }
-//            else
-//            {
-//                miCuenta = new Cuenta(nombre);
-//            }
-//            cuentas[count] = miCuenta;
-//            count++;
         
-//        Debug.Log("el banco esta lleno");
-//    }
-//}
+        //else
+        //{
+        //    miCuenta = new Cuenta(nombre);
+        //}
+        //cuentas[count] = miCuenta;
+        //count++;
+    }
+}

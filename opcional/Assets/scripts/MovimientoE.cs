82591cfdb2924f4b12e2a97f7e69402f133524c5
perrashijueputas2 <49006875+perrashijueputas2@users.noreply.github.com>
2019-06-08 11:31:39 -0500

    way+

---
+++ opcional/Assets/scripts/MovimientoE.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class MovimientoE : MonoBehaviour
+{
+    public List<GameObject> Waypoints = new List<GameObject>();
+
+    public Transform Point;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = Point.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
